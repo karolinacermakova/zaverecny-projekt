2ffd1a445690bcb57194d38c532bffe37030f0d1
Karolína Čermáková <karolinnacermakova@gmail.com>
2023-05-24 09:34:14 +0200

    layouts

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function(eleventyConfig) {
+
+	eleventyConfig.addPassthroughCopy("images");
+
+	eleventyConfig.addPassthroughCopy("css");
+
+  return {
+
+    templateFormats: ["njk", "html", "md", "liquid"],
+
+   
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
+
+};
